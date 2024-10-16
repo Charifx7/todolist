86029b2724dc9a59a6f199db792ae5ddf8602e96
Charifx7 <charif.edu7@gmail.com>
2024-10-16 17:12:54 +0700

    update tailwind

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import EditView from '../views/EditView.vue'
 import HomeView from '../views/HomeView.vue'
+import Testview from '@/views/Testview.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,11 @@ const router = createRouter({
       path: '/',
       name: 'todolist',
       component: HomeView
+    },
+    {
+      path: '/test',
+      name: 'test',
+      component: Testview
     },
     {
       path: '/todo/:id/edit',
